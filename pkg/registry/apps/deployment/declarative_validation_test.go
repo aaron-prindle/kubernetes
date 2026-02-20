f4e786910c109391ef7bc8e0388857e021d3fc90
Aaron Prindle <aprindle@google.com>
2026-02-20 04:51:53 +0000

    feat: Migrate DeploymentStrategy to use declarative validation
    Adds +k8s:discriminator and +k8s:member tags to DeploymentStrategy to migrate validation logic for the RollingUpdate field to use the declarative validation framework. Adds tests.

--- pkg/registry/apps/deployment/declarative_validation_test.go
+++ pkg/registry/apps/deployment/declarative_validation_test.go
@@ -1,12 +1,31 @@
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package deployment
 
 import (
 	"testing"
 
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/apimachinery/pkg/util/validation/field"
 	genericapirequest "k8s.io/apiserver/pkg/endpoints/request"
 	apitesting "k8s.io/kubernetes/pkg/api/testing"
 	"k8s.io/kubernetes/pkg/apis/apps"
+	api "k8s.io/kubernetes/pkg/apis/core"
 )
 
 func TestDeclarativeValidateForDeclarative(t *testing.T) {
@@ -18,7 +37,32 @@ func TestDeclarativeValidateForDeclarative(t *testing.T) {
 	testCases := map[string]struct {
 		input        apps.Deployment
 		expectedErrs field.ErrorList
-	}{}
+	}{
+		"valid RollingUpdate strategy": {
+			input: mkDeployment(apps.RollingUpdateDeploymentStrategyType, &apps.RollingUpdateDeployment{
+				MaxUnavailable: intstr.FromInt32(1),
+				MaxSurge:       intstr.FromInt32(1),
+			}),
+		},
+		"valid Recreate strategy": {
+			input: mkDeployment(apps.RecreateDeploymentStrategyType, nil),
+		},
+		"invalid RollingUpdate strategy missing rollingUpdate": {
+			input: mkDeployment(apps.RollingUpdateDeploymentStrategyType, nil),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("spec", "strategy", "rollingUpdate"), "this should be defaulted and never be nil"),
+			},
+		},
+		"invalid Recreate strategy with rollingUpdate": {
+			input: mkDeployment(apps.RecreateDeploymentStrategyType, &apps.RollingUpdateDeployment{
+				MaxUnavailable: intstr.FromInt32(1),
+				MaxSurge:       intstr.FromInt32(1),
+			}),
+			expectedErrs: field.ErrorList{
+				field.Forbidden(field.NewPath("spec", "strategy", "rollingUpdate"), "may not be specified when strategy `type` is 'Recreate'"),
+			},
+		},
+	}
 	for k, tc := range testCases {
 		t.Run(k, func(t *testing.T) {
 			apitesting.VerifyValidationEquivalence(t, ctx, &tc.input, Strategy.Validate, tc.expectedErrs)
@@ -36,12 +80,75 @@ func TestDeclarativeValidateUpdateForDeclarative(t *testing.T) {
 		oldObj       apps.Deployment
 		updateObj    apps.Deployment
 		expectedErrs field.ErrorList
-	}{}
+	}{
+		"valid update RollingUpdate to Recreate": {
+			oldObj: mkDeployment(apps.RollingUpdateDeploymentStrategyType, &apps.RollingUpdateDeployment{
+				MaxUnavailable: intstr.FromInt32(1),
+				MaxSurge:       intstr.FromInt32(1),
+			}),
+			updateObj: mkDeployment(apps.RecreateDeploymentStrategyType, nil),
+		},
+		"valid update Recreate to RollingUpdate": {
+			oldObj: mkDeployment(apps.RecreateDeploymentStrategyType, nil),
+			updateObj: mkDeployment(apps.RollingUpdateDeploymentStrategyType, &apps.RollingUpdateDeployment{
+				MaxUnavailable: intstr.FromInt32(1),
+				MaxSurge:       intstr.FromInt32(1),
+			}),
+		},
+		"invalid update Recreate with rollingUpdate": {
+			oldObj: mkDeployment(apps.RecreateDeploymentStrategyType, nil),
+			updateObj: mkDeployment(apps.RecreateDeploymentStrategyType, &apps.RollingUpdateDeployment{
+				MaxUnavailable: intstr.FromInt32(1),
+				MaxSurge:       intstr.FromInt32(1),
+			}),
+			expectedErrs: field.ErrorList{
+				field.Forbidden(field.NewPath("spec", "strategy", "rollingUpdate"), "may not be specified when strategy `type` is 'Recreate'"),
+			},
+		},
+		"invalid update RollingUpdate missing rollingUpdate": {
+			oldObj:    mkDeployment(apps.RecreateDeploymentStrategyType, nil),
+			updateObj: mkDeployment(apps.RollingUpdateDeploymentStrategyType, nil),
+			expectedErrs: field.ErrorList{
+				field.Required(field.NewPath("spec", "strategy", "rollingUpdate"), "this should be defaulted and never be nil"),
+			},
+		},
+	}
 	for k, tc := range testCases {
 		t.Run(k, func(t *testing.T) {
 			tc.oldObj.ResourceVersion = "1"
 			tc.updateObj.ResourceVersion = "1"
 			apitesting.VerifyUpdateValidationEquivalence(t, ctx, &tc.updateObj, &tc.oldObj, Strategy.ValidateUpdate, tc.expectedErrs)
 		})
+	}
+}
+func mkDeployment(strategyType apps.DeploymentStrategyType, rollingUpdate *apps.RollingUpdateDeployment) apps.Deployment {
+	return apps.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-deployment",
+			Namespace: "test-namespace",
+		},
+		Spec: apps.DeploymentSpec{
+			Replicas: 1,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: map[string]string{"a": "b"},
+			},
+			Template: api.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{"a": "b"},
+				},
+				Spec: api.PodSpec{
+					RestartPolicy:                 api.RestartPolicyAlways,
+					TerminationGracePeriodSeconds: func(i int64) *int64 { return &i }(30),
+					DNSPolicy:                     api.DNSClusterFirst,
+					Containers: []api.Container{
+						{Name: "c", Image: "img", TerminationMessagePolicy: api.TerminationMessageFallbackToLogsOnError, ImagePullPolicy: api.PullIfNotPresent},
+					},
+				},
+			},
+			Strategy: apps.DeploymentStrategy{
+				Type:          strategyType,
+				RollingUpdate: rollingUpdate,
+			},
+		},
 	}
 }
